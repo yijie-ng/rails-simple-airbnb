aeef852264324128ff7071c6c4a42f1a8252a6a4
Yi Jie Ng ngyijie92@gmail.com <ngyijie92@gmail.com>
2021-11-17 23:57:34 +0800

    Seeded fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'cleaning database...'
+Flat.destroy_all
+
+puts 'creating flats...'
+
+4.times do
+  flat = Flat.create!(
+    name: Faker::Company.name,
+    address: Faker::Address.street_address,
+    description: Faker::Lorem.paragraph,
+    price_per_night: rand(150..500),
+    number_of_guests: rand(1..5)
+  )
+  puts "#{flat.id} is created."
+end
+
+puts 'finished!'
